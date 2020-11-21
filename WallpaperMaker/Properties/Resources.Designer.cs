48d0ac469ef78a66479925e37efb05da307081e6
Mark Clark <markjclark@outlook.com>
2020-11-21 09:41:29 -0500

    Renamed WinForms project;

--- WallpaperMaker/Properties/Resources.Designer.cs
+++ WallpaperMaker/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace WallpaperMaker.Properties {
+namespace WallpaperMaker.WinForm.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace WallpaperMaker.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("WallpaperMaker.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("WallpaperMaker.WinForm.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
--- WallpaperMaker/Properties/Settings.Designer.cs
+++ WallpaperMaker/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace WallpaperMaker.Properties {
+namespace WallpaperMaker.WinForm.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
